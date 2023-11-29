c319fbd181067ae09767184cd795a71b7adc566a
Papaya4head <133377767+Papaya4head@users.noreply.github.com>
2023-11-29 12:14:03 +0300

    Add files via upload

---
+++ django task 2.py
@@ -0,0 +1,46 @@
+def prompt():
+    para=input('enter a paragraph: ')
+    return para
+def token(para):
+    words=para.split()
+    return words
+def freq(words):
+    freq={}
+    for word in words:
+        if word not in freq:
+            freq[word]=0
+        freq[word]+=1
+    return freq
+def display(freq):
+    for word,freq in sorted(freq.items(),key=lambda item:item[1],reverse=True):
+        print(f'{word}: {freq}')
+def ser(word,words):
+    freq=words.count(word)
+    if freq>0:
+        print(f"this word {word} appears {freq} times")
+    else:
+        print(f'this {word} dose not exist')
+def display_word_frequencies(word_frequencies, descending=True, top_n=None):
+    """Displays the word frequencies in descending order. Optionally, only displays the top N most frequent words."""
+    if descending:
+        sorted_word_frequencies = sorted(word_frequencies.items(), key=lambda item: item[1], reverse=True)
+    else:
+        sorted_word_frequencies = sorted(word_frequencies.items(), key=lambda item: item[1])
+
+    if top_n:
+        sorted_word_frequencies = sorted_word_frequencies[:top_n]
+
+    for word, frequency in sorted_word_frequencies:
+        print(f"{word}: {frequency}")
+Para=prompt()
+Words=token(Para)
+freq=freq(Words)
+
+print('disiplaying all freq :')
+display(freq)
+
+search_word = input("\nEnter a word to search for: ")
+ser(search_word,Words)
+
+print("\nDisplaying the top 5 most frequent words:")
+display_word_frequencies(freq, top_n=5)
