f16316dd73554d78d56efe3b8a2d69cd9eac4e23
papya4head <eyoseyasephrem@gmail.com>
2023-11-29 07:34:07 +0300

    first submission

---
+++ gdsc django.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+x = 123
+print(x)
+y=456
+print(y)
+x,y=y,x
+print(x)
+print(y)
+
+
+# In[3]:
+
+
+x="papaya"
+print(x)
+
+
+# In[6]:
+
+
+x = 123
+print(type(x))
+
+
+# In[7]:
+
+
+import os
+
+
+# In[ ]:
+
+
+# print(12)
+
+"""
+x = 1
+int
+print(type(x))
+x = "abc"
+print(type(x))
+str
+x = False
+print(type(x))
+bool
+x = None
+print(type(x))
+none
+"""
+x = "abc"
+
+for i in x:
+    print(i)
+
+    
+
+
+# In[9]:
+
+
+help("keywords")
+
+
+# In[15]:
+
+
+x=(2)
+y= int(input('number '))
+d= x*y
+print(d)
+
+
+# In[16]:
+
+
+x=7
+x +=1
+print(x)
+
+
+# In[17]:
+
+
+x =7
+print(type(x))
+x+=1
+print(x)
+y=("dog")
+print(type(y))
+y*=2
+print(y)
+z= 4.5
+print(type(z))
+z*=2
+print(z)
+o = True
+print(type(o))
+
+#eyoseyas 
+
+
+# In[ ]:
+
+
+x=int(input("a"))
+y=int(input("b"))
+print(x*y)
+
+
+# In[ ]:
+
+
+x=int(input("a"))
+y=int(input("b"))
+print(x>y)
+
+
+
+# In[18]:
+
+
+x=float(input("a"))
+y=float(input("b"))
+print(x/y)
+
+
+# In[ ]:
+
+
+
+
